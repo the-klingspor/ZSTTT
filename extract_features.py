1f927853c0812a63cb85589c3b110e23ba80d480
the-klingspor <j.st@wwu.de>
2023-05-03 15:36:20 +0200

    [FIX] attribute to class mapping

--- extract_features.py
+++ extract_features.py
@@ -2,6 +2,7 @@ import torch
 import torchvision.transforms as transforms
 import argparse
 import numpy as np
+import timm
 
 from torch.utils.data import DataLoader
 from tqdm import tqdm
@@ -22,34 +23,8 @@ parser.add_argument('--class_txt', type=str, default='trainvalclasses.txt')
 parser.add_argument('--attribute_path', type=str, default='/mnt/qb/akata/jstrueber72/datasets/CUB/attributes/class_attribute_labels_continuous.txt')
 
 # Hyper parameters
-parser.add_argument('--architecture', type=str, default="resnet50")
-parser.add_argument('--epochs', type=int, default=90)
-parser.add_argument('--batch_size', type=int, default=16)
-parser.add_argument('--learning_rate', type=float, default=5e-4)
-parser.add_argument('--momentum', type=float, default=0.9)
-parser.add_argument('--weight_decay', type=float, default=1e-4)
-parser.add_argument('--label_smoothing', type=float, default=0.1)
-parser.add_argument('--loss_weight', type=float, default=0.1)
 parser.add_argument('--image_size', type=int, default=336)
-
-## wandb
-parser.add_argument('--log_online', action='store_true',
-                    help='Flag. If set, run metrics are stored online in addition to offline logging. Should generally '
-                         'be set.')
-parser.add_argument('--wandb_key', default='65954b19f28cc0f35372188d50be8f11cdb79321', type=str, help='API key for W&B.')
-parser.add_argument('--project', default='Sample_Project', type=str,
-                    help='Name of the project - relates to W&B project names. In --savename default setting part of '
-                         'the savename.')
-parser.add_argument('--group', default='', type=str, help='Name of the group - relates to W&B group names - all runs '
-                                                          'with same setup but different seeds are logged into one '
-                                                          'group. In --savename default setting part of the savename. '
-                                                          'Name is created as model_dataset_group')
-parser.add_argument('--savename', default='group_plus_seed', type=str, help='Run savename - if default, the savename'
-                                                                            ' will comprise the project and group name '
-                                                                            '(see wandb_parameters()).')
-parser.add_argument('--name_seed', type=str, default=0, help="Randomly generated code as name for the run.")
-parser.add_argument('--outname', help='folder to output data and model checkpoints')
-
+parser.add_argument('--architecture', type=str, default="resnet50")
 
 def extract_features(opt, model=None):
     # Hyperparameters
@@ -75,7 +50,10 @@ def extract_features(opt, model=None):
     # Load the pre-trained ResNet101 model from timm with additional rotation head
     if model == None:
         model = RotationNet(num_classes=dataset.num_classes, architecture=opt.architecture)
+        #model = timm.create_model(opt.architecture, pretrained=True, num_classes=4)
         model.load_state_dict(torch.load(opt.backbone_path))
+        #model.reset_classifier(0)
+        #model = timm.create_model(opt.architecture, pretrained=True, num_classes=0)
 
     model.to(device)
     model.eval()
@@ -97,6 +75,7 @@ def extract_features(opt, model=None):
         # Forward pass
         with torch.no_grad():
             batch_features = model.forward_features(images).cpu().numpy()
+            #batch_features = model(images).cpu().numpy()
 
         # Stack the features and labels into the output arrays
         end_idx = start_idx + len(images)
@@ -112,14 +91,6 @@ def extract_features(opt, model=None):
 def get_zsl_data_collection(opt):
     print("Start data collection.")
 
-    # Construct class to attribute mapping
-    attribute_mapping = {}
-    with open(opt.attribute_path, 'r') as f:
-        attributes = f.readlines()
-        for idx, attr_raw in enumerate(attributes):
-            attribute = np.fromstring(attr_raw, dtype=float, sep=' ')
-            attribute_mapping[float(idx)] = attribute
-
     # Initialize all data entries
     data = {}
     #data['images'] = np.array([])
@@ -130,7 +101,16 @@ def get_zsl_data_collection(opt):
     data['val_loc'] = np.array([])
     data['test_seen_loc'] = np.array([])
     data['test_unseen_loc'] = np.array([])
-    data['attributes'] = np.array([])
+    #data['attributes'] = np.array([])
+
+    # Construct class to attribute mapping
+    attribute_mapping = {}
+    with open(opt.attribute_path, 'r') as f:
+        raw_attributes = f.readlines()
+
+    attributes = [np.fromstring(attr, dtype=float, sep=' ') for attr in raw_attributes]
+
+    data['attributes'] = np.stack(attributes)
 
     # Classes used for training
     print("Collecting training data")
@@ -173,10 +153,6 @@ def get_zsl_data_collection(opt):
     test_idx = np.arange(start_test_loc, end_test_loc)
     data['test_unseen_loc'] = test_idx
     print("Done")
-
-    # Collect the attribute vector for each sample
-    #data['attributes'] = np.take(list(attribute_mapping.values()), data['labels'].astype(int), axis=0)
-    data['attributes'] = np.array([attribute_mapping[label] for label in data['labels']])
 
     return data
 
